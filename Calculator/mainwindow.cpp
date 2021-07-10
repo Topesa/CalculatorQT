87d972f2dfd289714c060e1f8a699aed10c115ef
Topesa <pesa710@gmail.com>
2021-07-10 21:54:07 +0200

    Disable window resize, set window icon, change label font size, add image for readme file

--- Calculator/mainwindow.cpp
+++ Calculator/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setFixedSize(this->size()); // disable window resize
 
     ui->display->setText(QString::number(calValue));
     QPushButton *numberButton[10];
